3b82d0a3228b7f1d0af1cd8a6f37c9074b45e762
Brian Michael <brianmichaeladero@gmail.com>
2024-08-23 06:45:39 +0300

    update

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -8,8 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://threads-m0a4.onrender.com"],
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -23,7 +24,7 @@ io.on("connection", (socket) => {
 
   const userId = socket.handshake.query.userId;
 
-  if (userId !== "undefined") userSocketMap[userId] = socket.id;
+  if (userId !== "undefined" && userId) userSocketMap[userId] = socket.id;
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("markMessageAsSeen", async ({ conversationId, userId }) => {

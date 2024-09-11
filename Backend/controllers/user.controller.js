d6eecc7ce5def51680e04f82afede475e38c98a6
Brian Michael <brianmichaeladero@gmail.com>
2024-09-11 16:38:42 +0300

    controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -213,3 +213,29 @@ export async function getUserProfile(req, res) {
     console.log("Error in getUserProfile: ", err.message);
   }
 }
+
+//!Getting Suggested Users
+export async function getSuggestedUsers(req, res) {
+  try {
+    //*get users that the user is not following only
+    const userId = req.user._id;
+    const usersFollowedByUser = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      { $match: { _id: { $ne: userId } } },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByUser.following.includes(user._id)
+    );
+    const suggestedUsers = filteredUsers.slice(0, 5);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in Getting suggested users");
+  }
+}
